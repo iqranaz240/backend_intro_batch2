3a9e5f27d1c211f3dce0a4518c6ee9066d1466cc
Bilal raza <attari1235@gmail.com>
2023-12-10 12:41:11 +0500

    initial commit

--- connectDb.js
+++ connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async function () {
     try {
-        await mongoose.connect('mongodb+srv://<add your mongo db connection string>');
+        await mongoose.connect('mongodb+srv://<username>:<password>@cluster0.uvfwj5u.mongodb.net/');
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
